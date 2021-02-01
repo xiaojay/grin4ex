547034b898ee546977b03347157b681c0c412d4d
xiaojay <xiaojay@gmail.com>
2021-02-01 13:14:43 +0800

    add proxy for grin v5

--- proxy4grin5.py
+++ proxy4grin5.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/deposit/<uid>/v2/foreign', methods=['GET', 'POST'])
+@app.route('/v2/foreign', methods=['GET', 'POST'])
 def index(uid):
     #put uid to db and associate with this tx.
     print('Request headers: {}'.format(request.headers))
@@ -14,7 +14,7 @@ def index(uid):
 
     if request.json.get('method') == 'receive_tx':
         slate = request.json['params'][0]
-        print(slate['id'])
+        #print(slate['id'])
         #print(slate['amount'])
         #put id\amount to db 
     url = 'http://127.0.0.1:3415/v2/foreign'
@@ -23,12 +23,12 @@ def index(uid):
         print(resp.json())
         slate = resp.json()
         #uxtos (inputs/outpus) for this tx
-        inputs = slate['result']['Ok']['tx']['body']['inputs']
-        outputs = slate['result']['Ok']['tx']['body']['outputs']
+        #inputs = slate['result']['Ok']['tx']['body']['inputs']
+        #outputs = slate['result']['Ok']['tx']['body']['outputs']
         #put the slate/inputs/outputs to db
     
     print('Response: {}'.format(resp.text))
     return resp.text
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='8080')
